5c4320b5fd06ed2387c360f0a1154cba20f2ddb8
Charles Moulliard <ch007m@gmail.com>
2014-07-02 18:01:15 +0200

    Add embedded hawtio demo

---
+++ demos/hawtio-embedded/src/main/java/my/cool/demo/hawtio/MainApp.java
@@ -0,0 +1,45 @@
+package my.cool.demo.hawtio;
+
+import io.fabric8.insight.log.log4j.Log4jLogQuery;
+import io.fabric8.insight.log.log4j.Log4jLogQueryMBean;
+import io.hawt.embedded.Main;
+
+import java.net.URL;
+import java.util.Properties;
+
+public class MainApp {
+
+    private static final String resource = "hawtio-default-1.4.4.war";
+
+    public static void main(String[] args) throws Exception {
+
+        MainApp myApp = new MainApp();
+        // Get location of the WAR
+        String location = myApp.findResource(resource).getPath();
+        // Configure settings
+        myApp.settings();
+
+        // Start Log4j QueryBean
+        Log4jLogQuery log4jQuery = new Log4jLogQuery();
+        log4jQuery.start();
+
+        Main main = new Main();
+        main.setWarLocation(location);
+        // Run a Jetty Web Server with hawtio war
+        main.run();
+    }
+
+    private URL findResource(String resource) {
+        URL url = getClass().getResource(resource);
+        return url;
+    }
+
+    private void settings() {
+        Properties props = System.getProperties();
+        props.setProperty("hawtio.authenticationEnabled","false");
+        props.setProperty("hawtio.config.repo","git@github.com:cmoulliard/hawtio-config.git");
+        props.setProperty("hawtio.offline","true"); // REQUIRE OTHERWISE LOG IS NOT WORKING !
+        props.setProperty("hawtio.dirname","/Users/chmoulli/Temp/hawtio/");
+    }
+
+}
